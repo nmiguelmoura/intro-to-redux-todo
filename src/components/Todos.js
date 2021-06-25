7b4ef49a6af207b925ffd2073b99bff950659328
Nuno Machado <nuno.machado@dunelm.com>
2021-06-25 11:49:52 +0100

    Add thunks

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -7,12 +7,16 @@ const Todos = () => {
   const todos = useSelector((state) => state);
   const dispatch = useDispatch();
 
+  React.useEffect(() => {
+    dispatch(actions.handleGetTodos())
+  }, [dispatch])
+
   const toggleTodoCompleteStatus = (id) => {
     dispatch(actions.toggleTodo(id));
   };
 
   const deleteTodo = (id) => {
-    dispatch(actions.deleteTodo(id));
+    dispatch(actions.handleDeleteTodo(id));
   };
 
   return (
